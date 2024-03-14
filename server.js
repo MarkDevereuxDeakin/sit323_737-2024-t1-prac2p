b8b4915699f92b15bd5088003b8c654afd979658
Mark Devereux <61493405+MarkDevereuxDeakin@users.noreply.github.com>
2024-03-14 20:27:34 +0800

    Updated server.js file comments modified:   server.js

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
+// Purpose: This is the main file for the microservice. It serves the index.html file as a simple webpage.
+
+//Imports the express module
 const express = require('express');
+//Creates an express application
 const app = express();
+//Sets the port to 8080
 const port = 8080;
 
 //Serves the index.html file as a simple webpage
@@ -7,6 +12,7 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
+//Starts the microservice listening on the assigned port (8080)
 app.listen(port, () => {
     console.log(`Microservice listening at http://localhost:${port}`);    
 });
